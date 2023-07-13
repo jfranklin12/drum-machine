0345a846d6809d63331a16e7dc2d85ba9c0595a5
jfranklin12 <j.franks725@gmail.com>
2023-07-13 14:53:19 -0700

    reworked file structure and intial layout

---
+++ src/components/DrumMachine.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const DrumMachine = () => {
+
+    return (
+        <div id="drum-machine">
+            <div id="display" className='drum-pad-container'>{drumSound}</div>
+            <div className='drum-pad-container'>
+                {/* map of drum pad buttons */}
+
+            </div>
+                
+            
+        </div>
+    )
+}
+
+export default DrumMachine;
---
+++ src/components/DrumPad.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const DrumPad = () => {
+
+    return (
+        <div className="drum-pad">
+
+        </div>
+    )
+}
+
+export default DrumPad;
