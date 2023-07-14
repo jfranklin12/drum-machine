87f7a4340966bea9f6c8ca6b3301933d5430f123
jfranklin12 <j.franks725@gmail.com>
2023-07-13 17:28:41 -0700

    base formating for drum machine and buttons

--- src/components/DrumMachine.js
+++ src/components/DrumMachine.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './drumMachine.css';
+import DrumPad from './DrumPad';
 
 const DrumMachine = () => {
 
     return (
         <div id="drum-machine">
-            <div id="display" className='drum-pad-container'>{/*drumSoundDescription*/}</div>
+            {/* <div id="display" className='drum-pad-container'>drumSoundDescription</div> */}
             <div className='drum-pad-container'>
                 {/* map of drum pad buttons */}
 
