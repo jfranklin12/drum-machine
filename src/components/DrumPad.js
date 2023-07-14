9c83057e94a331132776f1c6c7a1f4135629f3c0
jfranklin12 <j.franks725@gmail.com>
2023-07-14 09:33:30 -0700

    drum pad button plays sound clip on click

--- src/components/DrumPad.js
+++ src/components/DrumPad.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 const DrumPad = ({ id, text, audio }) => {
+    const audioRef = useRef(null);
+
+    const handleClick = () => {
+        audioRef.current.play();
+    };
 
     return (
-        <div className="drum-pad" id={id}>
+        <div className="drum-pad" id={id} onClick={handleClick}>
             {text}
-            <audio className='clip' src={audio} id={id}></audio>
+            <audio className='clip' src={audio} id={id} ref={audioRef}></audio>
 
         </div>
     )
